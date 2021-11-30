a84349f24595f9b7fed08ec1fd1b2240f45269ac
Lucas Mazaron <lucas.mazaron@objective.com.br>
2021-11-30 07:55:58 -0300

    Product Repository Refactory Completed

--- src/modules/products/domain/repositories/IProductsRepository.ts
+++ src/modules/products/domain/repositories/IProductsRepository.ts
@@ -1,8 +1,8 @@
-// import { ICreateProducts } from '../models/ICreateProducts';
+import { ICreateProducts } from '../models/ICreateProducts';
 import { IProduct } from '../models/IProduct';
 
 export interface IProductsRepository {
   findByName(name: string): Promise<IProduct | undefined>;
-  // create(data: ICreateProducts): Promise<IProduct>;
-  // save(product: IProduct): Promise<IProduct>;
+  create(data: ICreateProducts): Promise<IProduct>;
+  save(product: IProduct): Promise<IProduct>;
 }
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,12 +1,31 @@
 import { IProductsRepository } from './../../../domain/repositories/IProductsRepository';
 import { Product } from '../entities/Product';
 import { getRepository, Repository } from 'typeorm';
+import { ICreateProducts } from 'modules/products/domain/models/ICreateProducts';
 
 export class ProductRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
 
   constructor() {
     this.ormRepository = getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProducts): Promise<Product> {
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async save(product: Product): Promise<Product> {
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
