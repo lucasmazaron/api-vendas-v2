71acad4c6979582b41e695102c52c83311471e03
Lucas Mazaron <lucas.mazaron@objective.com.br>
2021-11-30 08:45:44 -0300

    Remove ListProduct

--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -1,19 +1,18 @@
 import { DeleteProductService } from '../../../services/DeleteProductService';
 import { Request, Response } from 'express';
 import { CreateProductService } from '../../../services/CreateProductService';
-import { ListProductService } from '../../../services/ListProductService';
 import { ShowProductService } from '../../../services/ShowProductService';
 import { UpdateProductService } from '../../../services/UpdateProductService';
 import { container } from 'tsyringe';
 
 export class ProductsController {
-  public async index(request: Request, response: Response): Promise<Response> {
-    const listProducts = new ListProductService();
+  // public async index(request: Request, response: Response): Promise<Response> {
+  //   const listProducts = new ListProductService();
 
-    const products = await listProducts.execute();
+  //   const products = await listProducts.execute();
 
-    return response.json(products);
-  }
+  //   return response.json(products);
+  // }
 
   public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
